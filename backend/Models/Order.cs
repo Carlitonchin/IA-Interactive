a001303770a0d15a67aca134841be4ed353da825
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-11 13:25:48 -0400

    delete orderProduct model and add this logic into Order model

--- backend/Models/Order.cs
+++ backend/Models/Order.cs
@@ -11,17 +11,30 @@ namespace backend.Models
         Canceled
     }
 
+    public class OrderProduct
+    {
+        public Product? product {get; private set;}
+        public int? cant {get; private set;}
+
+        public OrderProduct(Product? p, int? c)
+        {
+            this.product = p;
+            this.cant = c;
+        }
+    }
     public class Order : IValidable
     {
         public Order(int id, Status status)
         {
             this.Id = id;
             this.Status = status;
+            this.products = new List<OrderProduct>();
         }
 
         public int Id { get; private set; }
         public Status Status { get; set; }
 
+        public List<OrderProduct> products { get; set; }
         public Error? Validate()
         {
             return null;
