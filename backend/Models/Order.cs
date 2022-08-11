71654c86a119ecc03da567e7d3428c2d3fe07d4c
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-11 14:17:49 -0400

    Add some logic to validate duplicate products in a order

--- backend/Models/Order.cs
+++ backend/Models/Order.cs
@@ -13,14 +13,43 @@ namespace backend.Models
 
     public class OrderProduct
     {
-        public Product? product {get; private set;}
-        public int? cant {get; private set;}
-
-        public OrderProduct(Product? p, int? c)
+        public Product product {get; set;}
+        public int cant {get; private set;}
+        public OrderProduct(Product p, int c)
         {
             this.product = p;
             this.cant = c;
         }
+
+    }
+
+    public class ListOrderProduct
+    {
+        private List<OrderProduct> _list;
+        private Dictionary<string, bool> _skus;
+
+        public ListOrderProduct()
+        {
+            this._list = new List<OrderProduct>();
+            this._skus = new Dictionary<string, bool>();
+        }
+
+        public Error? Add(OrderProduct op)
+        {
+            if(this._skus.ContainsKey(op.product.SKU))
+                return new Error($"Product {op.product.Name} is 2 times in the order");
+
+            this._skus[op.product.SKU] = true;
+            this._list.Add(op);
+
+            return null;
+        }
+
+        public IEnumerable<OrderProduct> GetOrderProducts()
+        {
+            return this._list.AsEnumerable<OrderProduct>();
+        }
+
     }
     public class Order : IValidable
     {
@@ -28,13 +57,13 @@ namespace backend.Models
         {
             this.Id = id;
             this.Status = status;
-            this.products = new List<OrderProduct>();
+            this.products = new ListOrderProduct();
         }
 
         public int Id { get; private set; }
         public Status Status { get; set; }
 
-        public List<OrderProduct> products { get; set; }
+        public ListOrderProduct products { get; private set; }
         public Error? Validate()
         {
             return null;
