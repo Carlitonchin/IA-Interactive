c84ceea4321359a432a3e42abee328444c96bb0c
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-11 11:22:34 -0400

    uitl created for binding request body

---
+++ backend/Utils/Request.cs
@@ -0,0 +1,10 @@
+namespace backend.Utils
+{
+    // this file is for write class for binding body request
+    public class UpdateStockRequest
+    {
+        public string? sku {get;set;}
+        public int? cant {get;set;}
+    }
+
+}
