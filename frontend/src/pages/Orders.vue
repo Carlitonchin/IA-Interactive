93e954ab8abf6469274fa0e1d5addcc902051b08
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-12 14:17:18 -0400

    orders page finished

--- frontend/src/pages/Orders.vue
+++ frontend/src/pages/Orders.vue
@@ -1,11 +1,18 @@
 <script setup>
-    import {ref, onMounted} from 'vue'
+    import {ref, onMounted, computed} from 'vue'
     const url_get = "https://localhost:7009/orders"
     const url_change_status = "https://localhost:7009/orders/change-status/"
     const url_cancel = "https://localhost:7009/orders/cancel/"
 
     const orders = ref([])
     const error = ref("")
+    const filter = ref(-1)
+
+    const orders_filtered = computed(()=>
+    {
+        return orders.value.filter(o=>filter.value == -1 || o.status == filter.value)
+    })
+
     const order_status =
     {
         0:"Pending",
@@ -98,6 +105,12 @@
         )
         .catch(err=> error.value = err)
     }
+
+    function change_filter(e, f)
+    {
+        filter.value = f
+    }
+
 </script>
 
 <template>
@@ -107,8 +120,18 @@
     <a href="new.html">
         <div id="add-order" class="button">Add order</div>
     </a>
+    <div id="filters-container">
+        <div @click="change_filter(e, -1)" :class="filter == -1 ? 'filter-selected filter':'filter'">All</div>
+        <div
+        v-for="i in 5" :key="i"
+        :class="filter == i-1 ? 'filter-selected filter':'filter'"
+        @click="change_filter(e, i-1)"
+        >
+        {{order_status[i-1]}}
+        </div>
+    </div>
     <div id="container-orders">
-        <div class="order" v-for="item in orders" :key="item.id">
+        <div class="order" v-for="item in orders_filtered" :key="item.id">
         <div class="header-order">
             <div class="order-id">
                 Order #{{item.id}}
@@ -143,6 +166,35 @@
 </template>
 
 <style scoped>
+
+#filters-container
+{
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid black;
+    margin-bottom: 1rem;
+}
+.filter-selected
+{
+    font-weight: bold;
+    color: blue;
+    border-bottom: 4px solid blue;
+}
+
+.filter
+{
+width: 100%;
+padding-top: 0.5rem;
+padding-bottom: 0.5rem;
+text-align: center;
+cursor: pointer;
+}
+
+.filter:hover
+{
+    background: rgba(128, 128, 128, 0.303);
+}
+
 #add-order
 {
     width: fit-content;
