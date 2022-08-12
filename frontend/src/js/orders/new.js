a30bc1113efd2dfa01425829e2603ba0061b620f
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-12 14:23:31 -0400

    new order page created

---
+++ frontend/src/js/orders/new.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import NewOrder from '../../pages/NewOrder.vue'
+
+import '../../assets/main.css'
+createApp(NewOrder).mount('#new_order')
---
+++ frontend/src/pages/NewOrder.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>New Order weeeeyyy</h1>
+</template>
