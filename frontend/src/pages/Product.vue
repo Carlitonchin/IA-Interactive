e6ec1bf9a9e55ac04c2f1e5a5eb9e3342ed70595
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-11 21:09:31 -0400

    page products added

---
+++ frontend/src/pages/Product.vue
@@ -0,0 +1,9 @@
+<template>
+    <p><a href="/">Home</a> | Products</p>
+</template>
+
+<style scoped>
+a{
+    text-decoration: none;
+}
+</style>
---
+++ frontend/src/product.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import Product from './pages/Product.vue'
+
+createApp(Product).mount('#page_product')
